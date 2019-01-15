86987c2677c77ebd4478e082b77842e8b3b1b709
zhouyu <18612699953@163.com>
2019-01-15 19:27:26 +0800

    alipush
    commit

--- www/yaAliyunpush.js
+++ www/yaAliyunpush.js
@@ -1,4 +1,3 @@
-// test
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
@@ -20,18 +19,15 @@ var AliyunPush = {
     },
 
     /**
-     * 阿里云推送消息透传回调
-     * @param  {Function} successCallback 成功回调
-     * @return {void}  
-     */
-    onMessage: function (successCallback) {
-        this.callNative('onMessage', [], successCallback)
-    },
-    /**
      * 获取设备唯一标识deviceId，deviceId为阿里云移动推送过程中对设备的唯一标识（并不是设备UUID/UDID）
      * @param  {Function} successCallback 成功回调
      * @param  {Function} errorCallback   失败回调
      * @return {void}  
+     * 
+     * 示例
+     * window.AliyunPush.getRegisterId((data) => {
+         alert('推送注册Deviceid' + data);
+     });
      */
     getRegisterId: function (successCallback, errorCallback) {
         this.callNative('getRegisterId', [], successCallback);
@@ -78,6 +74,37 @@ var AliyunPush = {
      */
     listTags: function (successCallback, errorCallback) {
         this.callNative('listTags', [], successCallback)
+    },
+
+    /**
+     * 阿里云推送消息透传回调
+     * @param  {Function} successCallback 成功回调
+     * @return {void}  
+     * JS示例
+     * type: string 消息类型,title: string '阿里云推送',
+     * content: string '推送的内容',extra: string | Object < k, v > 外健, url: 路由
+     * window.AliyunPush.onMessage((res) => {
+         alert('成功:' + res.type + "," + res.title + "," + res.content);
+     });
+
+     *消息类型
+     * message: 透传消息，
+     * notification: 通知接收，
+     * notificationOpened: 通知点击，
+     * notificationReceived： 通知到达，
+     * notificationRemoved： 通知移除，
+     * notificationClickedWithNoAction： 通知到达，
+     * notificationReceivedInApp： 通知到达打开 app
+     * 
+     * 示例
+     * window.AliyunPush.onMessage((res) => {
+     *  if (res.type === notificationOpened) {
+     *    alert('点击通知了，请做相关操作')
+     * }
+     });
+     */
+    onMessage: function (successCallback) {
+        this.callNative('onMessage', [], successCallback)
     },
 
     AliyunPush: AliyunPush
